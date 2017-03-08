29aff433a168a07f15398e81138ae97ca6316260
v_huqingze <huqz@neusoft.com>
2017-03-08 15:24:01 +0800

    Web

--- src/main/java/org/seckill/dto/SeckillExecution.java
+++ src/main/java/org/seckill/dto/SeckillExecution.java
@@ -1,12 +1,13 @@
 package org.seckill.dto;
 
 import org.seckill.entity.SuccessKilled;
+import org.seckill.enums.SeckillStatusEnum;
 
 /**
  * 封装执行秒杀后的结果
  * Created by 胡庆泽 on 2017/2/22.
  */
-public class SeckillExcution {
+public class SeckillExecution {
 
     private long seckillId;
 
@@ -19,17 +20,17 @@ public class SeckillExcution {
     //秒杀成功对象
     private SuccessKilled successKilled;
 
-    public SeckillExcution(long seckillId, int status, String statusInfo, SuccessKilled successKilled) {
+    public SeckillExecution(long seckillId, SeckillStatusEnum seckillStatusEnum, SuccessKilled successKilled) {
         this.seckillId = seckillId;
-        this.status = status;
-        this.statusInfo = statusInfo;
+        this.status = seckillStatusEnum.getStatus();
+        this.statusInfo = seckillStatusEnum.getStatusInfo();
         this.successKilled = successKilled;
     }
 
-    public SeckillExcution(long seckillId, int status, String statusInfo) {
+    public SeckillExecution(long seckillId,SeckillStatusEnum seckillStatusEnum) {
         this.seckillId = seckillId;
-        this.status = status;
-        this.statusInfo = statusInfo;
+        this.status = seckillStatusEnum.getStatus();
+        this.statusInfo = seckillStatusEnum.getStatusInfo();
     }
 
     public long getSeckillId() {
@@ -62,5 +63,15 @@ public class SeckillExcution {
 
     public void setSuccessKilled(SuccessKilled successKilled) {
         this.successKilled = successKilled;
+    }
+
+    @Override
+    public String toString() {
+        return "SeckillExecution{" +
+                "seckillId=" + seckillId +
+                ", status=" + status +
+                ", statusInfo='" + statusInfo + '\'' +
+                ", successKilled=" + successKilled +
+                '}';
     }
 }
---
+++ src/main/java/org/seckill/web/SeckillController.java
@@ -0,0 +1,7 @@
+package org.seckill.web;
+
+/**
+ * Created by user on 2017/3/7.
+ */
+public class SeckillController {
+}
